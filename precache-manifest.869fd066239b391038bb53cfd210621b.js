34b136b5c023a2fc539c80d83706520ab4c1c296
Morten Nissov <s163962@student.dtu.dk>
2021-04-20 00:15:59 +0200

    Updates

---
+++ precache-manifest.869fd066239b391038bb53cfd210621b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f93752cd2dc901c4ae5",
+    "url": "/static/css/main.eef07741.chunk.css"
+  },
+  {
+    "revision": "5f93752cd2dc901c4ae5",
+    "url": "/static/js/main.5f93752c.chunk.js"
+  },
+  {
+    "revision": "0de767c2806e9217181c",
+    "url": "/static/js/1.0de767c2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2a189d5ffa9f19a376e0496c5211e599",
+    "url": "/static/media/buns.2a189d5f.jpg"
+  },
+  {
+    "revision": "9065bca087f92096056e5c73ddfddb9a",
+    "url": "/static/media/calculator.9065bca0.svg"
+  },
+  {
+    "revision": "3d4362af8d7f500b26f871f371858e4a",
+    "url": "/index.html"
+  }
+];
